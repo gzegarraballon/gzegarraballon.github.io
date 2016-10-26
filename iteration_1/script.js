177d297eadd108609e24798a8bd2e62e575d8fd7
gzegarraballon <zegag586@newschool.edu>
2016-10-26 13:19:19 -0400

    Retrying

---
+++ iteration_1/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.element').mouseover(function(){
+    $(this).css('background-color','black');
+  });
+$('.element').mouseout(function(){
+    $(this).css('background-color','#5050C4');
+  });
+});
